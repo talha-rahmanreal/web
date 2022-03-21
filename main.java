53e445c82293a49a7f9f087951827ff64e3ed105
talha-rahmanreal <54823400+talha-rahmanreal@users.noreply.github.com>
2022-03-21 21:44:56 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int a=10, b=11, c=5, d=5,e=6;
+        String x = "Talha", y="Rahman";
+        
+        System.out.println(a>b);
+        System.out.println(b>a);
+        System.out.println(a>b && b>a);
+        System.out.println(a>b || b>a);
+        System.out.println(c>=d);
+        System.out.println(c<=d);
+        System.out.println((a+b)*(c+d)/e);
+        System.out.println(x+a);
+        System.out.println(y+a);
+        System.out.println(x+y);
+        System.out.println(x+" "+y);
+        System.out.println(x+" "+y+b);
+    }
+}
