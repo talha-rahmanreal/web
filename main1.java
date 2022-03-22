17ee13c5c883d2a74d4d8af7ca95118a8c9e86f2
talha-rahmanreal <54823400+talha-rahmanreal@users.noreply.github.com>
2022-03-22 11:04:42 +0530

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,31 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Talha Rahman");
+        int a = 10;
+        short s = 15;
+        byte b = 11;
+        long l = 117;
+        double i = 10.5;
+        float f = 10.5f;
+        char ch = 'a';
+        boolean b1 = true;
+        int v;
+        int Var;
+        int isValid;
+        
+    //    System.out.println(a);
+        v = 100;
+        System.out.println(v);
+        System.out.println(f);
+        System.out.println(b1);
+        
+    }
+}
---
+++ main2.java
@@ -0,0 +1,20 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Talha Rahman");
+        int a = 3;
+        int b = 10;
+        float c = b/(2*a);
+        
+    //    System.out.println(a);
+        System.out.println(c);
+        
+    }
+}
